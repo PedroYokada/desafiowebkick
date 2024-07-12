8c0bf30af7b29c83fd24ad51911a818d8d17e3da
PedroYokada <pedro_hlyokada@outlook.com>
2024-07-12 19:03:30 -0300

    Ajustes responsividade tela inicial 19:02 - 12/07 - sexta-feira

--- telainicial/index.js
+++ telainicial/index.js
@@ -3,14 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const navInicial = document.querySelector(".nav-pagina-inicial");
 
   MenuPagina.addEventListener("click", function () {
-    navInicial.classList.toggle("show");
+      navInicial.classList.toggle("show");
   });
 });
-
-
-
-
-
 
 function acessorios() {
   window.location.href = "acessorios/index.html";
@@ -25,43 +20,42 @@ function cadastro() {
 }
 
 function logout() {
-  window.location.href = "../index.html"; 
+  window.location.href = "../index.html";
 }
-
 
 var index_fotos = 1;
 mostrar_fotos(index_fotos);
 
 function plus_fotos(n) {
-    mostrar_fotos(index_fotos += n);
+  mostrar_fotos(index_fotos += n);
 }
 
 function current_fotos(n) {
-    mostrar_fotos(index_fotos = n);
+  mostrar_fotos(index_fotos = n);
 }
 
 function mostrar_fotos(n) {
-    let i;
-    let fotos = document.getElementsByClassName("games");
-    let pontos = document.getElementsByClassName("ponto");
+  let i;
+  let fotos = document.getElementsByClassName("games");
+  let pontos = document.getElementsByClassName("ponto");
 
-    if (n > fotos.length) {
-        index_fotos = 1;
-    }
-    if (n < 1) {
-        index_fotos = fotos.length;
-    }
+  if (n > fotos.length) {
+      index_fotos = 1;
+  }
+  if (n < 1) {
+      index_fotos = fotos.length;
+  }
 
-    for (i = 0; i < fotos.length; i++) {
-        fotos[i].style.display = "none";
-        fotos[i].getElementsByTagName('img')[0].classList.remove('active');
-    }
+  for (i = 0; i < fotos.length; i++) {
+      fotos[i].style.display = "none";
+      fotos[i].getElementsByTagName('img')[0].classList.remove('active');
+  }
 
-    for (i = 0; i < pontos.length; i++) {
-        pontos[i].className = pontos[i].className.replace(" active", "");
-    }
+  for (i = 0; i < pontos.length; i++) {
+      pontos[i].className = pontos[i].className.replace(" active", "");
+  }
 
-    fotos[index_fotos - 1].style.display = "block";
-    fotos[index_fotos - 1].getElementsByTagName('img')[0].classList.add('active');
-    pontos[index_fotos - 1].className += " active";
+  fotos[index_fotos - 1].style.display = "block";
+  fotos[index_fotos - 1].getElementsByTagName('img')[0].classList.add('active');
+  pontos[index_fotos - 1].className += " active";
 }
