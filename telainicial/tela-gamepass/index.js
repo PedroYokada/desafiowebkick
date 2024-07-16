a47edf243226c6d36daec21e9760f635894439e7
PedroYokada <pedro_hlyokada@outlook.com>
2024-07-15 22:47:58 -0300

    Atualização da pagina telagamepass, inclusão do carrosel e ajustes no header

--- telainicial/tela-gamepass/index.js
+++ telainicial/tela-gamepass/index.js
@@ -1,4 +1,41 @@
 function voltar() {
   console.log("Redirecionando para a página de acessórios...");
-  window.location.href = "../../telainicial/index.html"; 
+  window.location.href = "../../telainicial/index.html";
+}
+
+var index_fotos = 1;
+mostrar_fotos(index_fotos);
+
+function plusSlides(n) {
+  mostrar_fotos((index_fotos += n));
+}
+
+function currentSlide(n) {
+  mostrar_fotos((index_fotos = n));
+}
+
+function mostrar_fotos(n) {
+  let i;
+  let fotos = document.getElementsByClassName("games");
+  let pontos = document.getElementsByClassName("ponto");
+
+  if (n > fotos.length) {
+    index_fotos = 1;
+  }
+  if (n < 1) {
+    index_fotos = fotos.length;
+  }
+
+  for (i = 0; i < fotos.length; i++) {
+    fotos[i].style.display = "none";
+    fotos[i].getElementsByTagName("img")[0].classList.remove("active");
+  }
+
+  for (i = 0; i < pontos.length; i++) {
+    pontos[i].className = pontos[i].className.replace(" active", "");
+  }
+
+  fotos[index_fotos - 1].style.display = "block";
+  fotos[index_fotos - 1].getElementsByTagName("img")[0].classList.add("active");
+  pontos[index_fotos - 1].className += " active";
 }
